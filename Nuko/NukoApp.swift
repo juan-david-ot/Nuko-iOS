0dc377fa44ea991a409fcaf5cf2a0bff9d532766
JuanDa <juandarm02@gmail.com>
2026-03-25 21:15:26 +0100

    Initial Commit

---
+++ Nuko/NukoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NukoApp.swift
+//  Nuko
+//
+//  Created by JuanDa on 25/3/26.
+//
+
+import SwiftUI
+
+@main
+struct NukoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
